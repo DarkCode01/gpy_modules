079b23fad038967aafb6eef9ae5dbdd6ca2f1b7b
Jose M. Segura P <j0s3s3gur4@gmail.com>
2018-01-03 16:09:13 +0000

    gpy.py edited online with Bitbucket Mejoras al estilo del codigo...

--- gpy.py
+++ gpy.py
@@ -21,42 +21,32 @@ class Gpy(object):
         try:
             # Creando directorios
             os.mkdir(self.path+"/.gpy")
-            os.mkdir(self.path+"/.gpy/files")  # Carpeta donde se guardaran las ramas...
+            os.mkdir(self.path+"/.gpy/files")  # Carpeta donde se guardaran las copias de los archivos...
             os.mkdir(self.path+"/.gpy/commits")  # Carpeta donde se guardaran los commits...
 
             # Creando archivo de la rurta
-            with open(self.path+"/.gpy/path", "w") as fl:
-                fl.write(self.path)
-
-            with open(self.path+"/.gpy/commits/commits", "w"):
-                pass
+            self.file_ope(".gpy/path", "w", self.path
+            self.file_ope("gpy/commits/commits", "w", "pass"):
 
             return "Init creado."
+        
         except FileExistsError as e:
             return "Repositorio ya inicializado..."
             
     
     def status(self):
-        r = []
-
-        with open(self.path+"/.gpy/path", "r") as fl:
-            files = fl.readlines()
-
-        files = [_.replace("\n", "") for _ in files[1:]]
-        tmp = {}
-        for x in files:
-            tmp[x] = self.clasify(x)[-1].split("\n")[1]
-
-        for y in tmp:
-            with open(y, "r") as fl:
-                datos = fl.read()
-            with open(self.path+"/.gpy/files/"+tmp[y], "r") as fl:
-                datos_c = fl.read()
-
+        result = []
+        files = self.file_ope(".gpy/path", "r").split("\n")
+        files = [file_.replace("\n", "") for file_ in files[1:]]
+        tmp = {file_: self.clasify(file_)[-1].split("\n")[1]}
+        for file_hash in tmp:
+            datos = fl.self.file_ope(file_hash, "r")
+            datos_c self.file_ope(".gpy/files/" + tmp[file_hash], "r")
+                
             if datos != datos_c:
-                r.append(y.split("/")[-1])
+                r.append(file_hash.split("/")[-1])
 
-        return (r, tmp)
+        return (result, tmp)
 
 
     def diff(self):
@@ -66,14 +56,14 @@ class Gpy(object):
             for key in tmp:
                 if key.split()[-1] not in changes:
                     changes[key.split("/")[-1]] = ""
-                with open(key, "r") as fl:
-                    datos = fl.readlines()
-                    datos = [_.replace("\n", "") for _ in datos]
-                    datos = [_ for _ in datos if _ ]
-                with open(self.path+"/.gpy/files/"+tmp[key], "r") as fl:
-                    datos_c = fl.readlines()
-                    datos_c = [_.replace("\n", "") for _ in datos_c]
-                    datos_c = [_ for _ in datos_c if _ ]
+
+                datos = self.file_ope(key, "").split("\n")
+                datos = [_.replace("\n", "") for _ in datos]
+                datos = [_ for _ in datos if _ ]
+
+                datos_c = self.file_ope(".gpy/files/" + tmp[key], "r").split("\n")
+                datos_c = [_.replace("\n", "") for _ in datos_c]
+                datos_c = [_ for _ in datos_c if _ ]
 
                 if len(datos) > len(datos_c):
                     for i, s in enumerate(datos):
@@ -91,38 +81,30 @@ class Gpy(object):
         return changes
 
     def log(self):
-        with open(self.path+"/.gpy/commits/commits", "r") as fl:
-            logs = fl.read()
-        return logs
+        commits = self.file_ope(".gpy/commits/commits", "r")
+        return commits
     
     
-    def review(self, file, id_commit):
-        ruta_absoluta = os.path.abspath(file)
-        if ruta_absoluta not in self.status()[0]:
+    def review(self, file_, id_commit):
+        ruta_absoluta_archivo = os.path.abspath(file_)
+        if ruta_absoluta_archivo not in self.status()[0]:
             try:
-                if os.path.isfile(ruta_absoluta):
-                    logs = self.clasify(ruta_absoluta)
+                if os.path.isfile(ruta_absoluta_archivo):
+                    logs = self.clasify(ruta_absoluta_archivo)
                     file_commit = ""
 
                     if len(logs) > 0:
                         if id_commit == "-n":
-                            
-                            with open(self.path+"/.gpy/files/"+logs[-1].split("\n")[1], "r") as fl:
-                                datos = fl.read()
-
-                            with open(ruta_absoluta, "w") as fl:
-                                fl.write(datos)
+                            datos = self.file_ope(".gpy/files/" + logs[-1].split("\n")[1], "r")
+                            self.file_ope(ruta_absoluta_archivo, "w", datos)
                         else:
-                            for _ in logs:
-                                if id_commit in _ and ruta_absoluta in _:
-                                    file_commit = _
+                            for log in logs:
+                                if id_commit in log and ruta_absoluta_archivo in _:
+                                    file_commit = log
 
                             if file_commit:
-                                with open(self.path+"/.gpy/files/"+id_commit, "r") as fl:
-                                    datos = fl.read()
-                                
-                                with open(ruta_absoluta, "w") as fl:
-                                    fl.write(datos) 
+                                datos = self.file_ope(".gpy/files/" + id_commit, "r")
+                                self.file_ope(ruta_absoluta_archivo, "w", datos) 
                                 
                             else:
                                 return "No coinciden el id y el archivo"
@@ -140,74 +122,74 @@ class Gpy(object):
         else:
             return "Debe incluir los cambios y los commit"
 
-    def add(self, file):
+    def add(self, file_):
         try:
-            ruta_absoluta = os.path.abspath(file)
+            ruta_absoluta_archivo = os.path.abspath(file_)
             commit_hash = sha1(str(self.t).encode("utf-8")).hexdigest()
-            logs_f = self.clasify(ruta_absoluta)
+            logs_f = self.clasify(ruta_absoluta_archivo)
             datos_b = ""
-
-            with open(self.path+"/.gpy/path", "r") as fl:
-                files = fl.readlines()
-            with open(self.path+"/.gpy/path", "a") as fl:
-                if ruta_absoluta not in files:
-                    fl.write("\n"+ruta_absoluta)
+            files = self.file_ope(".gpy/path", "r").split("\n")
+            
+            if ruta_absoluta_archivo not in files:
+                self.file_ope(".gpy/path", "a", ruta_absoluta_archivo)
 
             if len(logs_f) > 0:
                 ultimo_commit = logs_f[-1].split("\n")[1]
-                
-                with open(self.path+"/.gpy/files/"+ultimo_commit, "r") as fl:
-                    datos_b = fl.read()
+                datos_b = self.file_ope(".gpy/files/" + ultimo_commit, "r")
                
-            if os.path.isfile(ruta_absoluta):
-                with open(ruta_absoluta, "r") as fl:
-                    datos = fl.read()
-
-
+            if os.path.isfile(ruta_absoluta_archivo):
+                datos = self.file_ope(ruta_absoluta_absoluta, "r")
+                    
                 if datos_b:
                     if datos_b != datos:
-                        with open(self.path+"/.gpy/files/"+commit_hash, "w") as fl:
-                            fl.write(datos)
-                    
-                        self.commit(ruta_absoluta, logs_f, commit_hash)
+                        self.file_ope(".gpy/files/" + commit_hash, "w", datos)
+                        self.commit(ruta_absoluta_archivo, logs_f, commit_hash)
                 else:
-                    with open(self.path+"/.gpy/files/"+commit_hash, "w") as fl:
-                        fl.write(datos)
-                    
-                    self.commit(ruta_absoluta, logs_f, commit_hash)
+                    self.file_ope(".gpy/files/" + commit_hash, "w", datos)
+                    self.commit(ruta_absoluta_archivo, logs_f, commit_hash)
                 
                 return "Anadido"
                     
             else:
                 return "Tiene que ser un archivo"
+        
         except FileNotFoundError as e:
             return "Repositorio no inicializado"
 
-    def commit(self, ruta_absoluta, logs_f, commit_hash):
-        m = str(input("Escribe el mensaje del commit: "))
+
+    def commit(self, ruta_absoluta_archivo, logs_f, commit_hash):
+        mensaje_commit = str(input("Escribe el mensaje del commit: "))
+        
         commit = "\n{0}\n{2} | {1} ({5})\n Path: {4}\n{3}\n\t".format(
             commit_hash,
             self.time_,
             self.user,
-            m,
-            ruta_absoluta,
+            mensaje_commit,
+            ruta_absoluta_archivo,
             len(logs_f)+1)
         
-        with open(self.path+"/.gpy/commits/commits", "a") as fl:
-            fl.write(commit)
+        self.file_ope(".gpy/commits/commits", "a", commit)
         
-        print("\n"+commit+"\n")
+        print("\n" + commit + "\n")
 
 
-    def clasify(self, file):
-        logs = []
-        for _ in self.log().split("\t"):
-            if file in _:
-                logs.append(_)
-        return logs
-
-
-
+    def clasify(self, file_):
+        commits = []
+        for commit in self.log().split("\t"):
+            if file_ in commit:
+                logs.append(commit)
+        return commits
+    
+    def file_ope(self, file_, argv):
+        ruta_absoluta_archivo = os.path.abspath(file_)
+        with open(ruta_absoluta_archivo, argv, string=None) as fl:
+            if string:
+                fl.write(string if string != "pass" else "")
+            elif argv in ("r"):
+                result = fl.read()
+        return result
+        
+        
 if __name__ == "__main__":
     gpy = Gpy()
     
